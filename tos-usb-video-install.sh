834751a7b5d755458e4fc1b509b3b37caf136cd8
minimaded <66683862+minimaded@users.noreply.github.com>
2022-01-09 15:24:35 +0000

    Add files via upload

--- tos-usb-video-install.sh
+++ tos-usb-video-install.sh
@@ -18,7 +18,7 @@ os_version() {
 }
 
 stop_clean() {
-    if [ sudo /bin/systemctl is-active -q tos-usb-video.service ] ; then
+    if [ "$(/bin/systemctl is-active -q tos-usb-video.service)" ] ; then
         echo "Stopping tos-usb-video.service"
         echo
         sudo /bin/systemctl stop tos-usb-video.service || install_notdone
